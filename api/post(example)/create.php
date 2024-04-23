4d3d4a99f7335a1a95ea15a6426efd2b818c4ee9
StefanSetiadi <stefansetiadi452002@gmail.com>
2024-04-23 11:35:55 +0700

    update_name

--- api/post(example)/create.php
+++ api/post(example)/create.php
@@ -2,11 +2,11 @@
   // Headers
   header('Access-Control-Allow-Origin: *');
   header('Content-Type: application/json');
-  header('Access-Control-Allow-Methods: PUT');
+  header('Access-Control-Allow-Methods: POST');
   header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With');
 
   include_once '../../config/Database.php';
-  include_once '../../models/Post.php';
+  include_once '../../models/Post(example).php';
 
   // Instantiate DB & connect
   $database = new Database();
@@ -18,22 +18,19 @@
   // Get raw posted data
   $data = json_decode(file_get_contents("php://input"));
 
-  // Set ID to update
-  $post->id = $data->id;
-
   $post->title = $data->title;
   $post->body = $data->body;
   $post->author = $data->author;
   $post->category_id = $data->category_id;
 
-  // Update post
-  if($post->update()) {
+  // Create post
+  if($post->create()) {
     echo json_encode(
-      array('message' => 'Post Updated')
+      array('message' => 'Post Created')
     );
   } else {
     echo json_encode(
-      array('message' => 'Post Not Updated')
+      array('message' => 'Post Not Created')
     );
   }
 
